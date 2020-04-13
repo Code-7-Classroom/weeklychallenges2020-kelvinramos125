eed8827a67ce11719c1b86e5b3ace99eb4a515bd
R2H <R2H@adminisatorsmbp.attlocal.net>
2020-04-13 17:06:30 -0400

    checkpoint2

---
+++ checkpoint2/script.js
@@ -0,0 +1,20 @@
+const me = (name)=>{
+    this.name = "Kelvin Melendez";
+}
+console.log(name);
+
+const sayHi = ()=>{
+    console.log(`Hi, ${name}`);
+}
+
+sayHi();
+
+// const me = (name, sayHi)=>{
+//     this.name= "Kelvin Melendez";
+//     console.log(name);
+
+//     this.sayHi = ()=>{
+//         console.log(`Hi, ${name}`);
+//     }
+// }
+// console.log(me);
