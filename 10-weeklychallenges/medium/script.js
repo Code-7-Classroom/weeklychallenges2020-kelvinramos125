b712b293b92cde791dc72b712d03f98e4fc3b30e
R2H <R2H@adminisatorsmbp.attlocal.net>
2020-04-16 10:44:24 -0400

    New 10 challenges

--- 10-weeklychallenges/medium/script.js
+++ 10-weeklychallenges/medium/script.js
@@ -1,26 +1,16 @@
-let array = [1, 3, 5, 2, 9, 4];
+let array = [];
 
-const avgNumber = () =>{
+const avgNumber = (array) =>{
     let sum = array.reduce(function (a, b) {
         return a + b;
     }, 0);
     console.log(array);
     console.log(`The total sum of the array is: ${sum}`);
-    console.log(`The average of the array is: ${sum/array.length}`);
+    console.log(`The average of the array is: ${(sum/array.length).toFixed(2)}`);
 }
 
-avgNumber();
+avgNumber([1, 3, 5, 2, 9, 4]);
 
+avgNumber([1, 3, 5, 2, 9, 4]);
 
-let array2 = [1, 3, 5, 2, 10, 4];
-
-const avgNumber2 = () =>{
-    let sum = array2.reduce(function (a, b) {
-        return a + b;
-    }, 0);
-    console.log(array);
-    console.log(`The total sum of the array is: ${sum}`);
-    console.log(`The average of the array is: ${(sum/array2.length).toFixed(2)}`);
-}
-
-avgNumber2();
+avgNumber([2, 5]);
