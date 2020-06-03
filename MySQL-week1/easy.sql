f7f1529b3714c0e662132557c764d977a28bcd66
kelvinramos125 <61425230+kelvinramos125@users.noreply.github.com>
2020-06-02 23:04:07 -0400

    Update easy.sql

--- MySQL-week1/easy.sql
+++ MySQL-week1/easy.sql
@@ -1,18 +1,21 @@
 DROP DATABASE IF EXISTS easy;
 CREATE DATABASE easy;
 USE easy;
-CREATE TABLE faveBook (title varchar(255), 
-publishDate varchar(255), 
-authorName varchar(255));
+CREATE TABLE faveBooks (title varchar(255), 
+publishDate YEAR, 
+authorFirstName VARCHAR(255),
+authorLastName VARCHAR(255));
 
-INSERT INTO faveBook (title , 
+INSERT INTO faveBooks (title ,
 publishDate , 
-authorName)
-VALUES("House of the Scorpion", "2002", "Nancy Farmer"),
-("The Hunger Games", "September 14, 2008", "Suzanne Collins"),
-("Perks of Being a Wallflower", "February 1, 1999", "Stephen Chbosky"),
-("The Diary Of a Young Girl", "June 25, 1947", "Anne Frank"), 
-("Moby Dick", "October 18, 1851", "Herman Melville"),
-("The Heretic’s Daughter", "September 3, 2008", "Kathleen Kent");
+authorFirstName, authorLastName)
+VALUES("House of the Scorpion", 2002, "Nancy", "Farmer"),
+("The Hunger Games", 2008, "Suzanne", "Collins"),
+("Perks of Being a Wallflower", 1999, "Stephen", "Chbosky"),
+("The Diary Of a Young Girl", 1947, "Anne", "Frank"), 
+("Moby Dick", 1951, "Herman", "Melville");
 
-SELECT * FROM faveBook;
+DELETE FROM faveBooks ORDER BY publishDate ASC LIMIT 1;
+SELECT COUNT(1) FROM faveBooks;
+SELECT * FROM faveBooks;
+DROP TABLE faveBooks;
