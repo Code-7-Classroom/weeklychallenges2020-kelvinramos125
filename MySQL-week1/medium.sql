68f415158a39fe058a3dd12ebdba1fac73a4483e
kelvinramos125 <61425230+kelvinramos125@users.noreply.github.com>
2020-06-02 23:04:39 -0400

    Update medium.sql

--- MySQL-week1/medium.sql
+++ MySQL-week1/medium.sql
@@ -1,20 +1,24 @@
 DROP DATABASE IF EXISTS mediumChallenge;
 CREATE DATABASE mediumChallenge;
 USE mediumChallenge;
-CREATE TABLE topMovies (title varchar(255), 
-releaseDate varchar(255), 
-rating varchar(255));
+CREATE TABLE topMovies (title VARCHAR(255), 
+releaseDate YEAR, 
+rating DECIMAL(2,1));
 
 INSERT INTO topMovies (title , 
 releaseDate , 
 rating)
-VALUES("Avengers: Ultron", "May 1, 2015", "PG-13"), 
-("Transcendence", "April 10, 2014", "PG-13"),
-("Godzilla: King of the Monsters", "May 31, 2019", "PG-13"),
-("Godzilla", "May 16, 2014", "PG-13"),
-("The Great Gatsby", "May 1, 2013", "PG-13"),
-("Pirates of the Caribbean: The Curse of the Black Pearl", "June 28, 2003", "PG-13"),
-("Lucy", "July 25, 2014", "R"),
-("Jurassic Park 3", "July 16, 2001", "PG-13"),
-("Interstellar", "October 26, 2014", "PG-13"),
-("The Perks of Being a Wallflower", "September 21, 2012", "PG-13");
+VALUES("Avengers: Ultron", 2015, 7.5), 
+("Transcendence", 2014, 2.0),
+("Godzilla: King of the Monsters", 2019, 4.2),
+("Godzilla", 2014, 7.5),
+("The Great Gatsby", 2013, 4.8),
+("Pirates of the Caribbean: The Curse of the Black Pearl", 2003, 7.9),
+("Lucy", 2014, 6.7),
+("Jurassic Park 3", 2001, 4.9),
+("Interstellar", 2014, 7.2),
+("Joker", 2019, 8.5);
+
+SELECT * FROM topMovies 
+WHERE title LIKE '%s%' ORDER BY releaseDate ASC;
+DROP TABLE topMovies;
