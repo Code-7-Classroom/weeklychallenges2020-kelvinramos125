daa9d670fd9ed9b1449ff3d805f097b6e4ef1f8e
kelvinramos125 <61425230+kelvinramos125@users.noreply.github.com>
2020-06-02 23:03:22 -0400

    Update veryEasy.sql

--- MySQL-week1/veryEasy.sql
+++ MySQL-week1/veryEasy.sql
@@ -3,11 +3,20 @@ CREATE DATABASE veryEasy;
 USE veryEasy;
 CREATE TABLE topCars (make varchar(255), 
 modle varchar(255), 
-carYear int);
+carYear YEAR);
 
-INSERT INTO topCars (make varchar, 
-modle varchar, 
+INSERT INTO topCars(make, 
+modle, 
 carYear)
 VALUES("BMW", "5 Series", 2020),
 ("Cadillac", "CT6", 2020),
 ("Chevrolet", "Camaro", 2020);
+
+INSERT INTO topCars(make, 
+modle, 
+carYear)
+VALUES("Toyota", "Corolla", 2020),
+("Subaru", "Impreza", 2020);
+
+SELECT * FROM topCars;
+DROP TABLE topCars;
